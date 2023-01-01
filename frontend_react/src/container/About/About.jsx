fd6104a36980c9e84e7a9214d1436a0786073bec
mackmraz <mackmraz@gmail.com>
2022-12-31 22:30:34 -0500

    Changes about section to link with backend sanity

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -2,17 +2,19 @@ import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 
 import { images } from '../../constants';
-
 import './About.scss';
-
-const abouts = [
-  { title: 'Web Development', description: 'I am a good web developer.', imgUrl: images.about01},
-  { title: 'Frontend Development', description: 'I am a good web developer.', imgUrl:images.about02},
-  { title: 'Backend Development', description: 'I am a good web developer.', imgUrl:images.about03},
-  { title: 'MERN Stack', description: 'I am a good web developer.', imgUrl:images.about04}
-];
+import { urlFor, client } from '../../client';
 
 const About = () => {
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query)
+    .then((data) => setAbouts(data))
+    }, []);
+  
   return (
     <>
       <h2 className="head-text">I Know That <span>Good Apps</span><br />means <span>Good Business</span></h2>
@@ -25,7 +27,7 @@ const About = () => {
         className="app__profile-item"
         key={about.title + index}
       >
-        <img src={about.imgUrl} alt={about.title} />
+        <img src={urlFor(about.imgUrl)} alt={about.title} />
         <h2 className="bold-text" style={{ marginTop: 20 }}>{about.title}</h2>
         <p className="p-text" style={{ marginTop: 10 }}>{about.description}</p>
       </motion.div>
