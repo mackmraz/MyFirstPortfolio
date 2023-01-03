d5faa1d7de15694545e67d4897db19546653002a
mackmraz <mackmraz@gmail.com>
2023-01-03 11:53:17 -0500

    Sets up Work.jsx, fixes bug with undefined variable

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { AiFillEye, AiFillGithub } from 'react-icons/ai';
+import { motion } from 'framer-motion';
 
+import { AppWrap, MotionWrap } from '../../wrapper';
+import { urlFor, client } from '../../client';
 import './Work.scss'
 
 const Work = () => {
+  const [works, setWorks] = useState([]);
+  const [filterWork, setFilterWork] = useState([]);
+  const [activeFilter, setActiveFilter] = useState('All');
+  const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1 });
+  
+  const handleWorkFilter = (item) => {
+    setActiveFilter(item);
+    setAnimateCard([{ y: 100, opacity: 0 }]);
+  }
+
   return (
-    <div>Work</div>
+    <>
+     <h2 className="head-text">My Creative <span>Portfolio</span> Section</h2>
+    <div className="app__work-filter">
+      {['UI/UX', 'Web App', 'Mobile App', 'React JS', 'All'].map((item, index) => (
+        <div
+        key={index}
+        onClick={() => handleWorkFilter(item)}
+        className={`app__work-filter-item app__flex p-text ${activeFilter === item ? 'item-active' : ''}`}
+        >
+          {item}
+        </div>
+      ))}
+    </div>
+    </>
   )
 }
 
-export default Work
+export default AppWrap(Work, 'work')
