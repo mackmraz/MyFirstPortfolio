d0343c001abe392db28239fc4b426c045890ecf2
mackmraz <mackmraz@gmail.com>
2023-10-17 22:17:36 -0400

    Fix icons and components. Turned off unused navigation dot.

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { BsTwitter, BsInstagram } from 'react-icons/bs';
+import { BsInstagram, BsLinkedin } from 'react-icons/bs';
 import { FaFacebookF } from 'react-icons/fa';
 
 const SocialMedia = () => {
   return (
     <div className="app__social">
         <div>
-            <BsTwitter />
+            <BsLinkedin />
         </div>
         <div>
             <FaFacebookF />
